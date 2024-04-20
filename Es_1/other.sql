48bda9daefb2369d2e851aa0544f66214d5f75bc
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2024-04-20 12:05:15 +0200

    Update other.sql

--- Es_1/other.sql
+++ Es_1/other.sql
@@ -1,66 +1,64 @@
---Creare il DB
+-- Creare il DB
 CREATE DATABASE uni_db;
 
---Controllare di averlo creato
+-- Controllare di averlo creato
 SHOW DATABASES;
 
---Selezionare il database
+-- Selezionare il database
 USE uni_db;
 
---Mostrare le tabelle create
+-- Mostrare le tabelle create
 SHOW TABLES;
 
---disattivare un vincolo
+-- disattivare un vincolo
 SET nomeVincolovoto = 0
 
---riattivare un vincolo
+-- riattivare un vincolo
 SET nomeVincolo = 1
 
---per un vincolo check
+-- per un vincolo check
 CONTRAINT nomeVincolo CHECK ...
 
---Disattivare i vincoli di integrità referenziale
-SET
-    foreign_key_checks = 0;
+-- Disattivare i vincoli di integrità referenziale
+SET foreign_key_checks = 0;
 
---Attivare i vincoli di integrità referenziale
-SET
-    foreign_key_checks = 1;
+-- Attivare i vincoli di integrità referenziale
+SET foreign_key_checks = 1;
 
---Testiamo i vincoli
---Codice fiscale già esistente
+-- Testiamo i vincoli
+-- Codice fiscale già esistente
 INSERT INTO
     studenti
 VALUES
     ("IN0500308", "Mario", "Rossi", "PNNGNN96L17123X");
 
---Testiamo i vincoli
---Un esame di un corso che non esiste 
+-- Testiamo i vincoli
+-- Un esame di un corso che non esiste 
 INSERT INTO
     esami
 VALUES
     ("111AA", "IN0500308", "2019-06-03", 25, NULL);
 
---Testiamo i vincoli
---Un 18 e lode 
+-- Testiamo i vincoli
+-- Un 18 e lode 
 INSERT INTO
     esami
 VALUES
     ("511SM", "IN0500308", "2019-06-03", 18, TRUE);
 
---Update della tabella
+-- Update della tabella
 ALTER TABLE studenti
 ADD COLUMN genere CHAR(1) NOT NULL;
 
---Update
+-- Update
 SET SQL_SAFE_UPDATES = 0;
 UPDATE studenti SET genere = "M";
 UPDATE studenti SET genere = "F"
 WHERE SUBSTRING(cf, 10, 1) BETWEEN "4" AND "7";
 SET SQL_SAFE_UPDATES = 1;
 
---Update tabella aggiunta vincolo
+-- Update tabella aggiunta vincolo
 ALTER TABLE studenti ADD CHECK (genere IN ("M", "F"));
 
---delete DB
+-- Delete DB
 DROP DATABASE uni_db;
