69e3e0c439764ed21ac90759ea1a8e52f119b949
Giovanni Pinna <giovanni.pinna.17l96@gmail.com>
2024-05-07 13:42:33 +0200

    Update trigger.sql

--- Es_3/trigger.sql
+++ Es_3/trigger.sql
@@ -6,7 +6,8 @@ CREATE TABLE
         data_assunzione DATE
     );
 
--- Trigger 1
+-- Trigger 1: Scrivere un trigger per tenere traccia delle assunzioni. 
+-- Supporre che l’inserimento di un docente nel DB coincida con la sua data di assunzione.
 -- ???
 -- SELECT * FROM assunzioni;
 -- INSERT INTO	professori (nome, cognome, cf, settore) VALUES ("NNome", "CCognome", "CCGNNM24A41L424I", "ING-INF/00");
